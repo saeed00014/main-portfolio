a947412e553fe84e8fed6ef4302a33e9e09d5edc
saeed <saeed00014@gmail.com>
2023-08-07 17:55:03 +0330

    css detailes changed

---
+++ src/components/SideLoadingCommponent/loading.jsx
@@ -0,0 +1,13 @@
+import './loading.css'
+
+import React from 'react'
+
+const SideLoadingCommponent = () => {
+  return (
+    <div className='SideLoadingContainer'>
+      <p><span></span> Loading</p> 
+    </div>
+  )
+}
+
+export default SideLoadingCommponent
